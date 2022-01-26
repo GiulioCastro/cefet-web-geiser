200aacb036f4e1a9717e68951457a9a05eed31a7
Giulio Lelis <giuliolsc1405@gmail.com>
2022-01-25 22:15:00 -0300

    Exercício 3

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ const app = express();
 const PORT = 3000
 const db = {}
 
-
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 1-4 linhas de código (você deve usar o módulo de filesystem (fs))
@@ -18,7 +17,6 @@ db.jogadores = JSON.parse(jogadoresJSON);
 
 const jogosPorJogadorJSON = await readFile('server/data/jogosPorJogador.json')
 db.jogosPorJogador = JSON.parse(jogosPorJogadorJSON);
-
 
 
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
@@ -45,6 +43,32 @@ app.get("/", (req, res) => {
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter ~15 linhas de código
 
+app.get("/jogador/:numero_identificador", (req, res) => {
+    const steamid = req.params.numero_identificador;
+    const player = db.jogadores.players.find((player) => player.steamid === steamid);
+
+    if (!player) return res.status(404).send('Jogador não encontrado!');
+
+    const { game_count, games } = db.jogosPorJogador[steamid];
+
+    const not_played_count = games.filter(game => game.playtime_forever === 0).length;
+    const top5_played = games.sort((a, b) => b.playtime_forever - a.playtime_forever).slice(0,5);
+
+    const details = {
+        player,
+        game_count,
+        not_played_count,
+    };
+
+    details.top5_played = top5_played.map(game => ({
+        ...game,
+        playtime_forever: (game.playtime_forever / 60).toFixed(0)
+    }));
+
+    details.favorite_game = details.top5_played[0];
+
+    res.render("jogador.hbs", details);
+});
 
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
